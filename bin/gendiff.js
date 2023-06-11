79b1027162b564f09faf1ef31768108a7e82839d
Nikolai Gurov <gurov.nicolay@gmail.com>
2023-06-11 16:38:41 +0200

    Step 3.1 updated gendiff.js, created getDifference in index.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,16 @@
 #!/usr/bin/env node
-import genDiff from '../src/index.js';
+import { Command } from 'commander';
+import getDifference from '../src/index.js';
 
-genDiff();
+const program = new Command();
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(getDifference(filepath1, filepath2));
+  });
+
+program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import { program } from 'commander';
+import _ from 'lodash';
+import * as path from 'path';
+import * as fs from 'fs';
 
-const genDiff = () => {
-  program
-    .version('0.0.1')
-    .description('Compares two configuration files and shows a difference.')
-    .arguments('<filepath1> <filepath2>')
-    .option('-f, --format <type>', 'output format');
-  program.parse();
+const getDifference = (filepath1, filepath2) => {
+  const validPath1 = path.resolve(process.cwd(), filepath1);
+  const validPath2 = path.resolve(process.cwd(), filepath2);
+
+  const data1 = fs.readFileSync(validPath1);
+  const data2 = fs.readFileSync(validPath2);
+
+  const json1 = JSON.parse(data1);
+  const json2 = JSON.parse(data2);
+
+  const keys1 = _.keys(json1);
+  const keys2 = _.keys(json2);
+  const allKeys = _.union(keys1, keys2).sort();
 };
 
-export default genDiff;
+export default getDifference;
