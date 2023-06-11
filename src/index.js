10aa39c044fd42639a9a1c953bca553a585c79c7
Nikolai Gurov <gurov.nicolay@gmail.com>
2023-06-11 16:54:19 +0200

    Step 3.3 Completed func getDifference in index.js

--- src/index.js
+++ src/index.js
@@ -15,6 +15,37 @@ const getDifference = (filepath1, filepath2) => {
   const keys1 = _.keys(json1);
   const keys2 = _.keys(json2);
   const allKeys = _.union(keys1, keys2).sort();
+
+  const result = allKeys.reduce((acc, key) => {
+    const isProp1Exist = _.has(json1, key);
+    const isProp2Exist = _.has(json2, key);
+    const value1 = json1[key];
+    const value2 = json2[key];
+    const deletedKey = `- ${key}`;
+    const addedKey = `+ ${key}`;
+
+    if (isProp1Exist && isProp2Exist) {
+      if (value1 === value2) {
+        acc[`${' '.repeat(2)}${key}`] = value1; 
+      } else {
+        acc[deletedKey] = value1;
+        acc[addedKey] = value2;
+      }
+    }
+    if (!isProp2Exist) {
+      acc[deletedKey] = value1;
+    }
+    if (!isProp1Exist) {
+      acc[addedKey] = value2;
+    }
+
+    return acc;
+  }, {});
+
+  const stringifyResult = JSON.stringify(result, null, 2);
+  const unquoted = stringifyResult.replace(/"|,/g, '');
+
+  return unquoted;
 };
 
 export default getDifference;
